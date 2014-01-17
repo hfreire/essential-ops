d03b16d481ce3f3e2b833bd737a82c29ad7bdb9d
Hugo Freire <hfreire@exec.sh>
2014-01-18 00:17:56 +0100

    Re-factored some stuff in backup.sh and added a mysql backup script.

--- backup.sh
+++ backup.sh
@@ -1,78 +1,89 @@
 #!/bin/sh
-
 EXTENSION=".tar.gz"
 
 # print help
 print_help() {
-    echo "Usage: $0 <dir1 dir2...> <rotations> <backups dir> <name>"
+    echo "Usage: $0 -s <dir1 dir2...> -d <backups dir> -n <name> -r <number of rotations>"
 }
 
 # rotate
-rotate() {
-	number=$(ls $backups_dir/$name.*$EXTENSION 2>/dev/null | wc -l);
+rotate_backups() {
+    local destination=$1
+    local name=$2
+    local rotations=$3
+	local number=$(ls $destination/$name.*$EXTENSION 2>/dev/null | wc -l | sed -e 's/^[ \t]*//');
 
 	# no need to rotate
 	if [ "$number" -eq 0 ]; then
-		echo "No previous backups detected."
+		echo "No existent backups detected."
 		return
 	fi
 
-	# delete only 1 previous backup
+	# remove the oldest backup
 	if [ "$number" -eq "$rotations" ]; then
-		echo "Detected previous backups. Removing oldest one: $name.$number$EXTENSION"
-		rm -f $backups_dir/$name.$number$EXTENSION
+		echo "Removing backup $destination/$name.$number$EXTENSION"
+		rm -f $destination/$name.$number$EXTENSION
 		number=$(($number-1))
 	fi
 
-	# delete all number of backups over the number of rotations
+	# remove all number of backups over the number of rotations
 	if [ "$number" -gt "$rotations" ]; then
 		over=$(($number-$rotations))
 		i=0
 		until [ "$i" < "$over" ]; do
-			echo "Detected previous backups. Removing oldest one: $name.$number$EXTENSION"
-			rm -f $backups_dir/$name.$number$EXTENSION
+			echo "Removing $destination/$name.$number$EXTENSION"
+			rm -f $destination/$name.$number$EXTENSION
 			number=$(($number-1))
 			$(($i+1))
 		done
 	fi
-	echo $number
 
 	number=$(($number+1))
-	backups=$(ls -tr $backups_dir/$name.*$EXTENSION);
 
-	for backup in $backups; do
+	for backup in $(ls -tr $destination/$name.*$EXTENSION 2>/dev/null); do
 		echo "Renaming $backup to $name.$number$EXTENSION"
-		mv -f $backup $backups_dir/$name.$number$EXTENSION
+		mv -f $backup $destination/$name.$number$EXTENSION
 		number=$(($number-1))
 	done
 }
 
-# package
-package() {
-	tar -czPhf $name.1$EXTENSION $dirs
-	chmod 440 $name.1$EXTENSION
+# backup filesystem
+backup_filesystem() {
+    local sources=$1
+    local name=$2
+    local destination=$3
+
+    echo "Backing up $sources into $destination/$name.1$EXTENSION"
+	tar -czPhf $destination/$name.1$EXTENSION $sources
+	chmod 440 $destination/$name.1$EXTENSION
 }
 
-# check arguments
-if [ "$#" -ne 4 ]; then
-        print_help
-        exit 1
-fi
+while getopts ":s:d:r:n:" opt; do
+  case $opt in
+    s)
+        sources=$OPTARG
+        ;;
+    d)
+        destination=$OPTARG
+        ;;
+    r)
+        rotations=$OPTARG
+        ;;
+    n)
+        name=$OPTARG
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        ;;
+  esac
+done
 
-dirs=$1
-rotations=$2
+[[ -n "$sources" ]] &&
+[[ -n "$destination" ]] &&
+[[ -n "$rotations" ]]  &&
+[[ -n "$name" ]] || { print_help; exit 1; }
 
-if [ "$4" = "none" ]; then
-	backups_dir=$3
-	name=backup
-else
-	backups_dir=$3/$4
-	name=$4
-fi
-
-cd $backups_dir
-
-rotate
-package
+rotate_backups "$destination" "$name" "$rotations"
+backup_filesystem "$sources" "$name" "$destination"
 
 exit 0
---
+++ mysql_backup.sh
@@ -0,0 +1,120 @@
+#!/bin/sh
+EXTENSION=".tar.gz"
+
+MYSQL_BIN=$(which mysql)
+MYSQLDUMP_BIN=$(which mysqldump)
+
+# print help
+print_help() {
+    echo "Usage: $0 -u <mysql_user> -p <mysql_password> -h <mysql_host> -d <backups dir> -n <name> -r <number of rotations>"
+}
+
+# rotate backups
+rotate_backups() {
+    local destination=$1
+    local name=$2
+    local rotations=$3
+    local number=$(ls $destination/$name.*$EXTENSION 2>/dev/null | wc -l | sed -e 's/^[ \t]*//');
+
+    # no need to rotate
+    if [ "$number" -eq 0 ]; then
+        echo "No existent backups detected."
+        return
+    fi
+
+    # remove the oldest backup
+    if [ "$number" -eq "$rotations" ]; then
+        echo "Removing backup $destination/$name.$number$EXTENSION"
+        rm -f $destination/$name.$number$EXTENSION
+        number=$(($number-1))
+    fi
+
+    # remove all number of backups over the number of rotations
+    if [ "$number" -gt "$rotations" ]; then
+        over=$(($number-$rotations))
+        i=0
+        until [ "$i" < "$over" ]; do
+            echo "Removing $destination/$name.$number$EXTENSION"
+            rm -f $destination/$name.$number$EXTENSION
+            number=$(($number-1))
+            $(($i+1))
+        done
+    fi
+
+    number=$(($number+1))
+
+    for backup in $(ls -tr $destination/$name.*$EXTENSION 2>/dev/null); do
+        echo "Renaming $backup to $name.$number$EXTENSION"
+        mv -f $backup $destination/$name.$number$EXTENSION
+        number=$(($number-1))
+    done
+}
+
+backup_filesystem() {
+    local sources=$1
+    local name=$2
+    local destination=$3
+
+    echo "Backing up $sources into $destination/$name.1$EXTENSION"
+    tar -czPhf $destination/$name.1$EXTENSION $sources
+    chmod 440 $destination/$name.1$EXTENSION
+}
+
+# backup mysql
+backup_mysql() {
+    local user=$1
+    local password=$2
+    local host=$3
+    local destination=$4
+
+    databases=$($MYSQL_BIN -u devops -pGJnXpqUquLlH2hbO -h db.linkafy.com -A --skip-column-names -e"SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('information_schema','mysql', 'performance_schema', 'innodb', 'tmp')")
+
+    for database in $databases
+    do
+        $MYSQLDUMP_BIN -u $user -p$password -h $host --hex-blob --routines --triggers $database --set-gtid-purged=OFF | gzip > $destination/tables/$database.sql.gz &
+    done
+    wait
+}
+
+while getopts ":u:p:h:d:n:r:" opt; do
+  case $opt in
+    u)
+        user=$OPTARG
+        ;;
+    p)
+        password=$OPTARG
+        ;;
+    h)
+        host=$OPTARG
+        ;;
+    d)
+        destination=$OPTARG
+        ;;
+    n)
+        name=$OPTARG
+        ;;
+    r)
+        rotations=$OPTARG
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        ;;
+  esac
+done
+
+[[ -n "$user" ]] &&
+[[ -n "$password" ]] &&
+[[ -n "$host" ]]  &&
+[[ -n "$destination" ]]  &&
+[[ -n "$name" ]]  &&
+[[ -n "$rotations" ]] ||
+{ print_help; exit 1; }
+
+rotate_backups "$destination" "$name" "$rotations"
+
+mkdir -p $destination/$name/tables
+backup_mysql "$user" "$password" "$host" "$destination/$name"
+backup_filesystem "$destination/$name" "$name" "$destination"
+rm -rf $destination/$name
+
+exit 0
